242952bd11ee746e6ab8965f24ccfe206da3d3fd
kernamx <powen.chang1@gmail.com>
2017-05-15 18:18:23 -0700

    fix 1stAndPike

--- app.js
+++ app.js
@@ -3,16 +3,41 @@
 var math;
 
 var firstAndPike = {
+  name: '1st and Pike',
   MinCust: 23,
   MaxCust: 65,
   AvgCookieSale: 6.3,
   CustPerHr: function() {
     return math.floor((this.MaxCust - this.MinCust) * math.random() + 1);
   },
-  CookiePerCust: function() {
-    return this.CustPerHr * this.AvgCookieSale;
+  CookiePerCust: function sumHrTotal() {
+    for (var i=6; i < 12; i++) {
+      return this.CustPerHr * this.AvgCookieSale;
+    }
+    for (var i=1; i <= 8; i++) {
+      return this.CustPerHr * this.AvgCookieSale;
+    }
+  },
+  function shop() {
+    var container = document.createElement('div');
+    container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
+
+    var list = document.createElement('ul');
+    var listArr = [];
+
+    for (var i=0; i < salmonShop.length; i++) {
+      listArr.push('<li>' + salmonShop[i] + '</li>');
+    }
+
+    var full_list = listArr.join('');
+    list.innerHTML = full_list;
+    document.body.appendChild(list);
   }
+
+  shop();
 };
+
+
 
 // var seaTacAirport = {
 //   MinCust: 3,
@@ -67,7 +92,7 @@ function shop() {
   var container = document.createElement('div');
   container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
 
-  var list = document.createElement('ol');
+  var list = document.createElement('ul');
   var listArr = [];
 
   for (var i=0; i < salmonShop.length; i++) {
