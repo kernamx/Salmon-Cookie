2229bd32daad3af59ed37434fa8dd5edc7d188af
kernamx <powen.chang1@gmail.com>
2017-05-16 13:33:49 -0700

    asking questions

--- app.js
+++ app.js
@@ -10,25 +10,30 @@ var firstAndPike = {
   CustPerHr: function() {
     return math.floor((this.MaxCust - this.MinCust) * math.random() + 1);
   },
+
   CookiePerCust: function sumHrTotal() {
-    // for (var i=6; i <= 20; i++) {
-    //   return this.CustPerHr * this.AvgCookieSale;
+    var businessHR = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-      var container = document.createElement('div');
-      container.innerHTML = '<p>' + '1st and Pike' + '</p>';
-
-      var list = document.createElement('ul');
-      var listArr = [];
-
-//need make an array that shows opening hours and cookies sold
-      for (var i=6; i <= 20; i++) {
-        listArr.push('<li>' + )
-
+    for (var i=0; i <= 15; i++) {
+       return this.CustPerHr * this.AvgCookieSale;
     }
   },
-  function shop() {
+
+  var container = document.createElement('div');
+    container.innerHTML = '<p>' + '1st and Pike' + '</p>';
+
+  var list = document.createElement('ul');
+  var listArr = [];
+
+//need make an array that shows opening hours and cookies sold
+var businessHR = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+  for (var i=0; i <= 15; i++) {
+      listArr.push('<li>' + sumHrTotal[i] + '</li>');
+  }
+  function openHR() {
     var container = document.createElement('div');
-    container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
+    container.innerHTML = '<p>' + '1st And Pike ' + '</p>';
 
     var list = document.createElement('ul');
     var listArr = [];
