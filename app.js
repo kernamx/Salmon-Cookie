e77d8f72325bdb5db080a04f3dfd2ded6ba40094
kernamx <powen.chang1@gmail.com>
2017-05-16 15:31:46 -0700

    just commit

--- app.js
+++ app.js
@@ -1,54 +1,59 @@
-'user stricts';
+'use strict';
 
-var math;
+var location = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+
 
 var firstAndPike = {
   name: '1st and Pike',
-  MinCust: 23,
-  MaxCust: 65,
-  AvgCookieSale: 6.3,
-  CustPerHr: function() {
-    return math.floor((this.MaxCust - this.MinCust) * math.random() + 1);
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  cookiesEachHour: [],
+  custPerHr: function() {
+    return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
   },
-
-  CookiePerCust: function sumHrTotal() {
-    var businessHR = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
+//Calculate and store the amounts of avg cookies for each hour and random number of customers
+//build an equation inside the for loop.  usiing for loop to run the equation through each hour.
+  cookiePerCust: function() {
     for (var i=0; i <= 15; i++) {
-       return this.CustPerHr * this.AvgCookieSale;
+      this.cookiesEachHour.push(this.custPerHr() * this.avgCookieSale);
     }
   },
-
-  var container = document.createElement('div');
-    container.innerHTML = '<p>' + '1st and Pike' + '</p>';
-
-  var list = document.createElement('ul');
-  var listArr = [];
-
-//need make an array that shows opening hours and cookies sold
-var businessHR = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-  for (var i=0; i <= 15; i++) {
-      listArr.push('<li>' + sumHrTotal[i] + '</li>');
-  }
-  function openHR() {
-    var container = document.createElement('div');
-    container.innerHTML = '<p>' + '1st And Pike ' + '</p>';
-
-    var list = document.createElement('ul');
-    var listArr = [];
-
-    for (var i=0; i < salmonShop.length; i++) {
-      listArr.push('<li>' + salmonShop[i] + '</li>');
-    }
-
-    var full_list = listArr.join('');
-    list.innerHTML = full_list;
-    document.body.appendChild(list);
-  }
-
-  shop();
 };
+firstAndPike.cookiePerCust();
+
+//display the values of each array as unordered lists in the browser
+//using for loop to loop each hour and show as list on dom
+// function salmonShop(){
+//   var container = document.createElement('div');
+//   container.innerHTML = '<p>' + '1st and Pike' + '</p>';
+//
+//   var list = document.createElement('ol');
+//   var listArr = [];
+//
+//   for (var i=0; i <= 15; i++) {
+//     listArr.push('<li>' + firstAndPike.cookiePerCust[i] + '</li>');
+//   }
+//
+//   var full_list = listArr.join('');
+//   list.innerHTML = full_list;
+//   document.body.appendChild(list);
+// }
+
+// function openHR() {
+//   var container = document.createElement('div');
+//     container.innerHTML = '<p>' + '1st and Pike' + '</p>';
+//   var list = document.createElement('ul');
+//   var listArr = [];
+//
+//     for (var i=0; i <= 15; i++) {
+//         listArr.push('<li>' + cookiePerCust[i] +  '</li>');
+//     }
+//     var full_list = listArr.join('');
+//     list.innerHTML = full_list;
+//     document.body.appendChild(list);
+//   }
+
 
 
 
@@ -100,21 +105,19 @@ var businessHR = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2p
 //   }
 // };
 
-var salmonShop = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-function shop() {
-  var container = document.createElement('div');
-  container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
 
-  var list = document.createElement('ul');
-  var listArr = [];
-
-  for (var i=0; i < salmonShop.length; i++) {
-    listArr.push('<li>' + salmonShop[i] + '</li>');
-  }
-
-  var full_list = listArr.join('');
-  list.innerHTML = full_list;
-  document.body.appendChild(list);
-}
-
-shop();
+// function shop() {
+//   var container = document.createElement('div');
+//   container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
+//
+//   var list = document.createElement('ul');
+//   var listArr = [];
+//
+//   for (var i=0; i < salmonShop.length; i++) {
+//     listArr.push('<li>' + salmonShop[i] + '</li>');
+//   }
+//
+//   var full_list = listArr.join('');
+//   list.innerHTML = full_list;
+//   document.body.appendChild(list);
+// }
