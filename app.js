c4028dd319600000761134b64d1b854065179f17
kernamx <powen.chang1@gmail.com>
2017-05-17 16:53:40 -0700

    show hourly cookies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var time = ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var firstAndPike = {
   name: '1st and Pike',
@@ -8,10 +9,10 @@ var firstAndPike = {
   avgCookieSale: 6.3,
   cookiesEachHour: [],
   total: 0,
-  time: ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   custPerHr: function() {
     return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
   },
+
 //Calculate and store the amounts of avg cookies for each hour and random number of customers
 //build an equation inside the for loop.  usiing for loop to run the equation through each hour.
 
@@ -28,7 +29,7 @@ firstAndPike.cookiePerCust();
 
 //display the values of each array as unordered lists in the browser
 //using for loop to loop each hour and show as list on dom
-function salmonShop() {
+function firstPikeShop() {
   var container = document.createElement('div');
   container.innerHTML = '<p>' + firstAndPike.name + '</p>';
   document.body.appendChild(container);
@@ -37,15 +38,15 @@ function salmonShop() {
   var list_Arr = [];
 
   for (var i=0; i < 15; i++) {
-    list_Arr.push('<li>' + firstAndPike.time[i] + ': ' + firstAndPike.cookiesEachHour[i] + ' cookies' + '</li>');
-  }
+    list_Arr.push('<li>' + time[i] + ': ' + firstAndPike.cookiesEachHour[i] + ' cookies' + '</li>');
 
+  }
   var full_list = list_Arr.join('');
 
   list.innerHTML = full_list;
   document.body.appendChild(list);
 }
-salmonShop();
+firstPikeShop();
 
 
 
@@ -57,7 +58,6 @@ var seaTacAirport = {
   avgCookieSale: 1.2,
   cookiesEachHour: [],
   total: 0,
-  time: ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   custPerHr: function() {
     return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
   },
@@ -79,9 +79,8 @@ function seaTacShop() {
 
   var list = document.createElement('ul');
   var list_Arr = [];
-
   for (var i=0; i < 15; i++) {
-    list_Arr.push('<li>' + seaTacAirport.time[i] + ': ' + seaTacAirport.cookiesEachHour[i] + ' cookies' + '</li>');
+    list_Arr.push('<li>' + time[i] + ': ' + seaTacAirport.cookiesEachHour[i] + ' cookies' + '</li>');
   }
 
   var full_list = list_Arr.join('');
@@ -89,6 +88,7 @@ function seaTacShop() {
   list.innerHTML = full_list;
   document.body.appendChild(list);
 }
+
 seaTacShop();
 
 
@@ -101,7 +101,6 @@ var seattleCenter = {
   avgCookieSale: 3.7,
   cookiesEachHour: [],
   total: 0,
-  time: ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   custPerHr: function() {
     return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
   },
@@ -125,7 +124,7 @@ function seattleCentShop() {
   var list_Arr = [];
 
   for (var i=0; i < 15; i++) {
-    list_Arr.push('<li>' + seattleCenter.time[i] + ': ' + seattleCenter.cookiesEachHour[i] + ' cookies' + '</li>');
+    list_Arr.push('<li>' + time[i] + ': ' + seattleCenter.cookiesEachHour[i] + ' cookies' + '</li>');
   }
 
   var full_list = list_Arr.join('');
@@ -134,18 +133,10 @@ function seattleCentShop() {
   document.body.appendChild(list);
 }
 seattleCentShop();
-// var capitolHill = {
-//   MinCust: 20,
-//   MaxCust: 38,
-//   AvgCookieSale: 2.3,
-//   CustPerHr: function() {
-//     return math.floor((this.MaxCust - this.MinCust) * math.random() + 1);
-//   },
-//   CookiePerCust: function() {
-//     return this.CustPerHr * this.AvgCookieSale;
-//   }
-// };
-//
+
+
+
+
 var capitolHill = {
   name: 'CapitolHill',
   minCust: 20,
@@ -153,7 +144,6 @@ var capitolHill = {
   avgCookieSale: 2.3,
   cookiesEachHour: [],
   total: 0,
-  time: ['6am', '7am', '8am', '9am','10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   custPerHr: function() {
     return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
   },
@@ -177,7 +167,7 @@ function capitoHillShop() {
   var list_Arr = [];
 
   for (var i=0; i < 15; i++) {
-    list_Arr.push('<li>' + capitolHill.time[i] + ': ' + capitolHill.cookiesEachHour[i] + ' cookies' + '</li>');
+    list_Arr.push('<li>' + time[i] + ': ' + capitolHill.cookiesEachHour[i] + ' cookies' + '</li>');
   }
 
   var full_list = list_Arr.join('');
@@ -186,31 +176,46 @@ function capitoHillShop() {
   document.body.appendChild(list);
 }
 capitoHillShop();
-// var alki = {
-//   MinCust: 2,
-//   MaxCust: 16,
-//   AvgCookieSale: 4.6,
-//   CustPerHr: function() {
-//     return math.floor((this.MaxCust - this.MinCust) * math.random() + 1);
-//   },
-//   CookiePerCust: function() {
-//     return this.CustPerHr * this.AvgCookieSale;
-//   }
-// };
 
 
-// function shop() {
-//   var container = document.createElement('div');
-//   container.innerHTML = '<p>' + 'List of stores: ' + '</p>';
-//
-//   var list = document.createElement('ul');
-//   var listArr = [];
-//
-//   for (var i=0; i < salmonShop.length; i++) {
-//     listArr.push('<li>' + salmonShop[i] + '</li>');
-//   }
-//
-//   var full_list = listArr.join('');
-//   list.innerHTML = full_list;
-//   document.body.appendChild(list);
-// }
+
+
+var alki = {
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+  cookiesEachHour: [],
+  total: 0,
+  custPerHr: function() {
+    return Math.floor((this.maxCust - this.minCust) * Math.random() + 1);
+  },
+  cookiePerCust: function() {
+    for (var i=0; i < 15; i++) {
+      this.cookiesEachHour.push(Math.floor(this.custPerHr() * this.avgCookieSale));
+      var sumCookies = Math.floor(this.custPerHr() * this.avgCookieSale);
+      this.total = this.total + sumCookies;
+    }
+    this.total;
+  }
+};
+alki.cookiePerCust();
+
+function alkiShop() {
+  var container = document.createElement('div');
+  container.innerHTML = '<p>' + alki.name + '</p>';
+  document.body.appendChild(container);
+
+  var list = document.createElement('ul');
+  var list_Arr = [];
+
+  for (var i=0; i < 15; i++) {
+    list_Arr.push('<li>' + time[i] + ': ' + alki.cookiesEachHour[i] + ' cookies' + '</li>');
+  }
+
+  var full_list = list_Arr.join('');
+
+  list.innerHTML = full_list;
+  document.body.appendChild(list);
+}
+alkiShop();
